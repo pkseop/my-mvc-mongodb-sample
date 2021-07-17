5c45ce9fdb9ecbedb1185c5e4335a7e87feabcd0
Kyounseop Park <kysepark@gmail.com>
2021-07-17 23:27:43 +0900

    첫 커밋

---
+++ src/main/java/my/spring/sample/mvc/MyMvcSampleApplication.java
@@ -0,0 +1,13 @@
+package my.spring.sample.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMvcSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMvcSampleApplication.class, args);
+	}
+
+}
